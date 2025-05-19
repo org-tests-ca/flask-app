5ca96a7887346271a3bb10b80e6c5cc1809963d7
clecio.antao <clecio.antao@gmail.com>
2025-05-19 05:09:44 -0300

    testes rollback

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! Clecio Antao! 3',
+        'message': 'You are doing great, little human! Clecio Antao! roobback',
         'deployed_on': 'kubernetes'
     })
 
